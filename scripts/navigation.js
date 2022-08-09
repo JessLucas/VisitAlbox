b05d4902947fab196237960a3cf17d24c4d6589c
JessLucas <101952498+JessLucas@users.noreply.github.com>
2022-08-10 01:24:11 +0200

    Add files via upload

---
+++ scripts/navigation.js
@@ -0,0 +1,7 @@
+function openNavbar() {
+    document.getElementById("alboxNavigationMenu").style.width = "100%";
+}
+
+function closeNavbar(){
+    document.getElementById("alboxNavigationMenu").style.width = "0%";
+}
